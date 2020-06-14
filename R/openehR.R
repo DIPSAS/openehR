58d5e87ba62b6327691d70e3c3079c28d8f85775
Bjørn Fjukstad <bjorn@cs.uit.no>
2020-06-14 17:40:52 +0200

    only return data.frame on successful result
    We now throw an error if the HTTP POST fails for any reason. This way
    the caller does not have to check the `data.frame` which is returned
    afterwards.

--- R/openehR.R
+++ R/openehR.R
@@ -1,35 +1,54 @@
-library(httr)
-library(jsonlite)
-
 query <- function(url, query, config = list()) {
-  if(!endsWith(url, "/")) {
+  if (!endsWith(url, "/")) {
     url = paste0(url, "/")
   }
 
-  url = paste0(url, "query/aql/")
+  url = paste0(url, "query/aql")
 
-  response <- POST(url = url, body = list(q = query), encode = "json", config = config)
+  response <- httr::POST(
+    url = url,
+    body = list(q = query),
+    encode = "json",
+    config = config
+  )
 
-  if(http_error(response)){
+  if (http_error(response)) {
+    stop(
+      paste0(
+        "Could not execute query. HTTP ",
+        response$status_code,
+        "Response:\n",
+        httr::content(response)
+      )
+    )
     return(response)
   } else {
-    content <- content(response, as = "raw")
+    content <- httr::content(response, as = "raw")
 
-    if (content[1]==as.raw(0xef) & content[2]==as.raw(0xbb) & content[3]==as.raw(0xbf)) {
+    # Strip away UTF-8 BOM in response if present
+    if (content[1] == as.raw(0xef)
+        && content[2] == as.raw(0xbb)
+        && content[3] == as.raw(0xbf)) {
       content = rawToChar(content[-(1:3)])
     } else {
       content = rawToChar(content)
     }
 
-    resultSet <- fromJSON(content)
+    # Parse resultset json
+    resultSet <- jsonlite::fromJSON(content)
 
-    #Create data frame
-    rows = resultSet$rows;
+    # Create data frame and use the correct column names from the response
+    rows = resultSet$rows
     columnNames = resultSet$columns$name
     dataframe <- as.data.frame(rows)
-    colnames(dataframe) <- columnNames
+
+    # ResultSet could have 0 rows and the dataframe would be empty. Only assign
+    # column names if the resultset contains any data.
+    if (ncol(dataframe) > 0) {
+      colnames(dataframe) <- columnNames
+    }
 
     return(dataframe)
   }
-}
 
+}
